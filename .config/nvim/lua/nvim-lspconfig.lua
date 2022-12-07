ecfdc734915c126121ab7b30611028ff14b3173a
CooperCarnahan <coopcarn@gmail.com>
2022-12-07 14:19:13 -0600

    fix: Change nvim-lspconfig deprecated function call

--- .config/nvim/lua/nvim-lspconfig.lua
+++ .config/nvim/lua/nvim-lspconfig.lua
@@ -1,6 +1,6 @@
 -- Setup lspconfig.
 local nvim_lsp = require('lspconfig')
-local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
+local capabilities = require('cmp_nvim_lsp').default_capabilities(vim.lsp.protocol.make_client_capabilities())
 local opts = { noremap=true, silent=true }
 
 local on_attach = function(client, bufnr)
