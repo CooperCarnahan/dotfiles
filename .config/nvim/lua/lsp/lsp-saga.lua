f6483b6c3e64abf5cbd66621be0acd3f7255e41b
CooperCarnahan <coopcarn@gmail.com>
2022-12-29 00:23:24 -0600

    Clean up lsp-related keymaps

--- .config/nvim/lua/lsp/lsp-saga.lua
+++ .config/nvim/lua/lsp/lsp-saga.lua
@@ -1,63 +1,95 @@
 local keymap = vim.keymap.set
-local saga = require('lspsaga')
+local saga = require("lspsaga")
 
 saga.init_lsp_saga({
-  show_outline = {
-  win_position = 'right',
-  --set special filetype win that outline window split.like NvimTree neotree
-  -- defx, db_ui
-  win_with = ' ',
-  win_width = 50,
-  auto_enter = true,
-  auto_preview = true,
-  virt_text = '┃',
-  jump_key = '<CR>',
-  -- auto refresh when change buffer
-  auto_refresh = true,
-  },
+	show_outline = {
+		win_position = "right",
+		--set special filetype win that outline window split.like NvimTree neotree
+		-- defx, db_ui
+		win_with = " ",
+		win_width = 50,
+		auto_enter = true,
+		auto_preview = true,
+		virt_text = "┃",
+		jump_key = "<CR>",
+		-- auto refresh when change buffer
+		auto_refresh = true,
+	},
 })
 
 -- Lsp finder find the symbol definition implement reference
 -- if there is no implement it will hide
 -- when you use action in finder like open vsplit then you can
 -- use <C-t> to jump back
-keymap("n", "gh", "<cmd>Lspsaga lsp_finder<CR>", { silent = true })
+keymap("n", "gr", "<cmd>Lspsaga lsp_finder<CR>", { silent = true, desc = "[LspSaga] Open delcaration & references" })
 
 -- Code action
-keymap({"n","v"}, "<leader>ca", "<cmd>Lspsaga code_action<CR>", { silent = true })
+keymap(
+	{ "n", "v" },
+	"<leader>ca",
+	"<cmd>Lspsaga code_action<CR>",
+	{ silent = true, desc = "[LspSaga] Run code action" }
+)
 
 -- Rename
-keymap("n", "gr", "<cmd>Lspsaga rename<CR>", { silent = true })
+keymap("n", "<leader>rn", "<cmd>Lspsaga rename<CR>", { silent = true, desc = "[LspSaga] Interactive rename" })
 
 -- Peek Definition
--- you can edit the definition file in this flaotwindow
+-- you can edit the definition file in this floatwindow
 -- also support open/vsplit/etc operation check definition_action_keys
 -- support tagstack C-t jump back
-keymap("n", "gd", "<cmd>Lspsaga peek_definition<CR>", { silent = true })
+keymap("n", "gd", "<cmd>Lspsaga peek_definition<CR>", { silent = true, desc = "[LspSaga] Peek definition" })
 
 -- Show line diagnostics
-keymap("n", "<leader>cd", "<cmd>Lspsaga show_line_diagnostics<CR>", { silent = true })
+keymap(
+	"n",
+	"<leader>cd",
+	"<cmd>Lspsaga show_line_diagnostics<CR>",
+	{ silent = true, desc = "[LspSaga] Show line diagnostics" }
+)
 
 -- Show cursor diagnostics
-keymap("n", "<leader>cd", "<cmd>Lspsaga show_cursor_diagnostics<CR>", { silent = true })
+keymap(
+	"n",
+	"<leader>cd",
+	"<cmd>Lspsaga show_cursor_diagnostics<CR>",
+	{ silent = true, desc = "[LspSaga] Show cursor diagnostics" }
+)
 
 -- Diagnostic jump can use `<c-o>` to jump back
-keymap("n", "[e", "<cmd>Lspsaga diagnostic_jump_prev<CR>", { silent = true })
-keymap("n", "]e", "<cmd>Lspsaga diagnostic_jump_next<CR>", { silent = true })
+keymap(
+	"n",
+	"[e",
+	"<cmd>Lspsaga diagnostic_jump_prev<CR>",
+	{ silent = true, desc = "[LspSaga] Jump to previous diagnostic" }
+)
+
+keymap(
+	"n",
+	"]e",
+	"<cmd>Lspsaga diagnostic_jump_next<CR>",
+	{ silent = true, desc = "[LspSaga] Jump to next diagnostic" }
+)
 
 -- Only jump to error
 keymap("n", "[E", function()
-  require("lspsaga.diagnostic").goto_prev({ severity = vim.diagnostic.severity.ERROR })
-end, { silent = true })
+	require("lspsaga.diagnostic").goto_prev({ severity = vim.diagnostic.severity.ERROR })
+end, { silent = true, desc = "[LspSaga] Jump to prev error" })
+
 keymap("n", "]E", function()
-  require("lspsaga.diagnostic").goto_next({ severity = vim.diagnostic.severity.ERROR })
-end, { silent = true })
+	require("lspsaga.diagnostic").goto_next({ severity = vim.diagnostic.severity.ERROR })
+end, { silent = true, desc = "[LspSaga] Jump to next error" })
 
 -- Outline
-keymap("n","<leader>o", "<cmd>Lspsaga outline<CR>",{ silent = true })
+keymap("n", "<leader>o", "<cmd>Lspsaga outline<CR>", { silent = true, desc = "[LspSaga] Open LSP outline of buffer" })
 
 -- Hover Doc
-keymap("n", "K", "<cmd>Lspsaga hover_doc<CR>", { silent = true })
+keymap(
+	"n",
+	"K",
+	"<cmd>Lspsaga hover_doc<CR>",
+	{ silent = true, desc = "[LspSaga] Open documentation for symbol under cursor in a floating window" }
+)
 
 -- if you want to pass some cli command into a terminal you can do it like this
 -- open lazygit in lspsaga float terminal
--- .config/nvim/lua/nvim-lspconfig.lua
+++ .config/nvim/lua/nvim-lspconfig.lua
@@ -1,33 +1,33 @@
 -- Setup lspconfig.
-local nvim_lsp = require('lspconfig')
-local capabilities = require('cmp_nvim_lsp').default_capabilities(vim.lsp.protocol.make_client_capabilities())
-local opts = { noremap=true, silent=true }
+local nvim_lsp = require("lspconfig")
+local capabilities = require("cmp_nvim_lsp").default_capabilities(vim.lsp.protocol.make_client_capabilities())
+local opts = { noremap = true, silent = true }
 
 local on_attach = function(client, bufnr)
-  -- Enable completion triggered by <c-x><c-o>
-  vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
+	-- Enable completion triggered by <c-x><c-o>
+	vim.api.nvim_buf_set_option(bufnr, "omnifunc", "v:lua.vim.lsp.omnifunc")
 
-  -- Mappings.
-  -- See `:help vim.lsp.*` for documentation on any of the below functions
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<CR>', opts)
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>', opts)
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>wa', '<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>', opts)
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>wr', '<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>', opts)
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>wl', '<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>', opts)
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>D', '<cmd>lua vim.lsp.buf.type_definition()<CR>', opts)
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>rn', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>ca', '<cmd>lua vim.lsp.buf.code_action()<CR>', opts)
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', opts)
+	local nmap = function(map, cmd, opts)
+		vim.api.nvim_buf_set_keymap(bufnr, "n", map, "<cmd>lua " .. cmd .. "<cr>", opts)
+	end
+	-- Mappings.
+	-- See `:help vim.lsp.*` for documentation on any of the below functions
+	nmap("gD", "vim.lsp.buf.declaration()", opts)
+	nmap("gd", "vim.lsp.buf.definition()", opts)
+	nmap("gi", "vim.lsp.buf.implementation()", opts)
+	nmap("<space>wa", "vim.lsp.buf.add_workspace_folder()", opts)
+	nmap("<space>wr", "vim.lsp.buf.remove_workspace_folder()", opts)
+	nmap("<space>wl", "print(vim.inspect(vim.lsp.buf.list_workspace_folders()))", opts)
+	nmap("<space>D", " vim.lsp.buf.type_definition()", opts)
+	nmap("<space>ca", "vim.lsp.buf.code_action()", opts)
 end
 
-local servers = {'pyright', 'rust_analyzer', 'clangd', 'sumneko_lua', 'dartls', 'gopls'}
+local servers = { "pyright", "rust_analyzer", "clangd", "sumneko_lua", "dartls", "gopls" }
 for _, lsp in pairs(servers) do
-  require('lspconfig')[lsp].setup {
-    on_attach = on_attach,
-    capabilities = capabilities
-  }
+	require("lspconfig")[lsp].setup({
+		on_attach = on_attach,
+		capabilities = capabilities,
+	})
 end
 
 -- require('rust-tools').setup({on_attach = on_attach})
-
