39026399f2b8ac6891226702fba68b6455ea943e
CooperCarnahan <coopcarn@gmail.com>
2023-03-19 20:42:45 -0500

    feat: Add nvim-spectre search and replace plugin

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -77,6 +77,23 @@ return {
   { "APZelos/blamer.nvim",
     cmd = "BlamerToggle",
   },
+  { "windwp/nvim-spectre",
+    event = "VeryLazy",
+    config = function()
+      vim.keymap.set('n', '<leader>S', '<cmd>lua require("spectre").open()<CR>', {
+        desc = "Open Spectre"
+      })
+      vim.keymap.set('n', '<leader>Sw', '<cmd>lua require("spectre").open_visual({select_word=true})<CR>', {
+        desc = "Search current word"
+      })
+      vim.keymap.set('v', '<leader>Sw', '<esc><cmd>lua require("spectre").open_visual()<CR>', {
+        desc = "Search current word"
+      })
+      vim.keymap.set('n', '<leader>Sp', '<cmd>lua require("spectre").open_file_search({select_word=true})<CR>', {
+        desc = "Search on current file"
+      })
+    end
+  },
   { "m4xshen/autoclose.nvim",
     event = "VeryLazy",
     config = function()
