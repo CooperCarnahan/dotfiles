dbd889db7999f66f052eb24a9f93fddaf6c57907
CooperCarnahan <coopcarn@gmail.com>
2023-04-18 08:29:50 -0500

    wip: add rust-tools to support rust debugging, etc.

--- .config/nvim/lua/plugins/lsp/init.lua
+++ .config/nvim/lua/plugins/lsp/init.lua
@@ -3,6 +3,21 @@ local M = {
   event = "BufReadPre",
   dependencies = {
     "hrsh7th/cmp-nvim-lsp",
+    { "simrat39/rust-tools.nvim", config = function()
+      local rt = require("rust-tools")
+
+      rt.setup({
+        server = {
+          on_attach = function(_, bufnr)
+            -- Hover actions
+            vim.keymap.set("n", "<C-space>", rt.hover_actions.hover_actions, { buffer = bufnr })
+            -- Code action groups
+            vim.keymap.set("n", "<Leader>ca", rt.code_action_group.code_action_group, { buffer = bufnr })
+            require("plugins.lsp.keys").setup(_, bufnr)
+          end,
+        },
+      })
+    end }
   },
 }
 
@@ -45,17 +60,6 @@ function M.config()
     gopls = {},
     marksman = {},
     pyright = {},
-    rust_analyzer = {
-      settings = {
-        ["rust-analyzer"] = {
-          cargo = { allFeatures = true },
-          checkOnSave = {
-            command = "clippy",
-            extraArgs = { "--no-deps" },
-          },
-        },
-      },
-    },
     yamlls = {},
     sumneko_lua = {}
     -- tailwindcss = {},
