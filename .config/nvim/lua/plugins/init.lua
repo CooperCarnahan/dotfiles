a412b268217d204321bc312bde393c232adff334
CooperCarnahan <coopcarn@gmail.com>
2023-08-11 15:47:58 -0500

    chore(nvim): format some plugin files
    Autoformatting strikes again. Hard to tell all that changed here and tbh it's been a few weeks since these changes were made

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -1,134 +1,134 @@
 -- Plugins that don't have some grandiose config can go in here
 
 return {
-	"MunifTanjim/nui.nvim",
-	"folke/which-key.nvim",
-	-- { "nvim-tree/nvim-web-devicons",
-	--   config = { default = true }, },
-	{
-		"SmiteshP/nvim-navic",
-		config = function()
-			vim.g.navic_silence = true
-			require("nvim-navic").setup({ separator = " ", highlight = true, depth_limit = 5 })
-		end,
-	},
-	{
-		"numToStr/comment.nvim",
-		config = true,
-		event = "VeryLazy",
-	},
-	{
-		"voldikss/vim-floaterm",
-		config = function()
-			vim.api.nvim_set_var("floaterm_width", 0.8)
-			vim.api.nvim_set_var("floaterm_height", 0.8)
-		end,
-		event = "VeryLazy",
-	},
-	{
-		"michaelb/sniprun",
-		build = "bash ./install.sh",
-		config = true,
-		cmd = "SnipRun",
-	},
-	{
-		"kylechui/nvim-surround",
-		config = true,
-		commit = "ad56e62",
-		event = "VeryLazy",
-	},
-	{
-		"tpope/vim-repeat",
-		event = "VeryLazy",
-	},
-	{
-		"jeffkreeftmeijer/vim-numbertoggle",
-		event = "VeryLazy",
-	},
-	-- { "gbprod/cutlass.nvim",
-	--   event = "VeryLazy",
-	--   config = function()
-	--     require("cutlass").setup({ exclude = { "ns", "nS" }, })
-	--   end,
-	--   commit = "e607a",
-	--   pin = true },
-	{
-		"smjonas/inc-rename.nvim",
-		cmd = "IncRename",
-		config = true,
-	},
-	{
-		"jghauser/mkdir.nvim",
-		event = "VeryLazy",
-	},
-	{
-		"stevearc/overseer.nvim",
-		-- cmd = "OverseerRun",
-		event = "VeryLazy",
-		config = true,
-	},
-	{
-		"folke/twilight.nvim",
-		cmd = "Twilight",
-		config = true,
-	},
-	{
-		"vim-test/vim-test",
-		branch = "master",
-		commit = "ca25025",
-		event = "VeryLazy",
-		config = function()
-			vim.g["test#cpp#catch2#relToProject_build_dir"] = "Debug/bin"
-			-- vim.g['test#cpp#catch2#bin_dir'] = './bin/'
-		end,
-		dependencies = {
-			"skywind3000/asyncrun.vim",
-			-- 'neomake/neomake',
-			"tpope/vim-dispatch",
-		},
-	},
-	{
-		"APZelos/blamer.nvim",
-		cmd = "BlamerToggle",
-	},
-	{
-		"windwp/nvim-spectre",
-		event = "VeryLazy",
-		config = function()
-			vim.keymap.set("n", "<leader>S", '<cmd>lua require("spectre").open()<CR>', {
-				desc = "Open Spectre",
-			})
-			vim.keymap.set("n", "<leader>Sw", '<cmd>lua require("spectre").open_visual({select_word=true})<CR>', {
-				desc = "Search current word",
-			})
-			vim.keymap.set("v", "<leader>Sw", '<esc><cmd>lua require("spectre").open_visual()<CR>', {
-				desc = "Search current word",
-			})
-			vim.keymap.set("n", "<leader>Sp", '<cmd>lua require("spectre").open_file_search({select_word=true})<CR>', {
-				desc = "Search on current file",
-			})
-		end,
-	},
-	{
-		"mbbill/undotree",
-		event = "VeryLazy",
-		config = function()
-			vim.keymap.set("n", "<leader>ut", vim.cmd.UndotreeToggle)
-		end,
-	},
-	{
-		"christoomey/vim-tmux-navigator",
-		event = "VeryLazy",
-	},
-	{
-		"stevearc/oil.nvim",
-		opts = {},
-		-- Optional dependencies
-		dependencies = { "nvim-tree/nvim-web-devicons" },
-		event = "VeryLazy",
-		config = function()
-			require("oil").setup()
-			vim.keymap.set("n", "-", require("oil").open, { desc = "Open parent directory" })
-		end,
-	},
+  "MunifTanjim/nui.nvim",
+  "folke/which-key.nvim",
+  -- { "nvim-tree/nvim-web-devicons",
+  --   config = { default = true }, },
+  {
+    "SmiteshP/nvim-navic",
+    config = function()
+      vim.g.navic_silence = true
+      require("nvim-navic").setup({ separator = " ", highlight = true, depth_limit = 5 })
+    end,
+  },
+  {
+    "numToStr/comment.nvim",
+    config = true,
+    event = "VeryLazy",
+  },
+  {
+    "voldikss/vim-floaterm",
+    config = function()
+      vim.api.nvim_set_var("floaterm_width", 0.8)
+      vim.api.nvim_set_var("floaterm_height", 0.8)
+    end,
+    event = "VeryLazy",
+  },
+  {
+    "michaelb/sniprun",
+    build = "bash ./install.sh",
+    config = true,
+    cmd = "SnipRun",
+  },
+  {
+    "kylechui/nvim-surround",
+    config = true,
+    commit = "ad56e62",
+    event = "VeryLazy",
+  },
+  {
+    "tpope/vim-repeat",
+    event = "VeryLazy",
+  },
+  {
+    "jeffkreeftmeijer/vim-numbertoggle",
+    event = "VeryLazy",
+  },
+  -- { "gbprod/cutlass.nvim",
+  --   event = "VeryLazy",
+  --   config = function()
+  --     require("cutlass").setup({ exclude = { "ns", "nS" }, })
+  --   end,
+  --   commit = "e607a",
+  --   pin = true },
+  {
+    "smjonas/inc-rename.nvim",
+    cmd = "IncRename",
+    config = true,
+  },
+  {
+    "jghauser/mkdir.nvim",
+    event = "VeryLazy",
+  },
+  {
+    "stevearc/overseer.nvim",
+    -- cmd = "OverseerRun",
+    event = "VeryLazy",
+    config = true,
+  },
+  {
+    "folke/twilight.nvim",
+    cmd = "Twilight",
+    config = true,
+  },
+  {
+    "vim-test/vim-test",
+    branch = "master",
+    commit = "ca25025",
+    event = "VeryLazy",
+    config = function()
+      vim.g["test#cpp#catch2#relToProject_build_dir"] = "Debug/bin"
+      -- vim.g['test#cpp#catch2#bin_dir'] = './bin/'
+    end,
+    dependencies = {
+      "skywind3000/asyncrun.vim",
+      -- 'neomake/neomake',
+      "tpope/vim-dispatch",
+    },
+  },
+  {
+    "APZelos/blamer.nvim",
+    cmd = "BlamerToggle",
+  },
+  {
+    "windwp/nvim-spectre",
+    event = "VeryLazy",
+    config = function()
+      vim.keymap.set("n", "<leader>S", '<cmd>lua require("spectre").open()<CR>', {
+        desc = "Open Spectre",
+      })
+      vim.keymap.set("n", "<leader>Sw", '<cmd>lua require("spectre").open_visual({select_word=true})<CR>', {
+        desc = "Search current word",
+      })
+      vim.keymap.set("v", "<leader>Sw", '<esc><cmd>lua require("spectre").open_visual()<CR>', {
+        desc = "Search current word",
+      })
+      vim.keymap.set("n", "<leader>Sp", '<cmd>lua require("spectre").open_file_search({select_word=true})<CR>', {
+        desc = "Search on current file",
+      })
+    end,
+  },
+  {
+    "mbbill/undotree",
+    event = "VeryLazy",
+    config = function()
+      vim.keymap.set("n", "<leader>ut", vim.cmd.UndotreeToggle)
+    end,
+  },
+  {
+    "christoomey/vim-tmux-navigator",
+    event = "VeryLazy",
+  },
+  {
+    "stevearc/oil.nvim",
+    opts = {},
+    -- Optional dependencies
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    event = "VeryLazy",
+    config = function()
+      require("oil").setup()
+      vim.keymap.set("n", "-", require("oil").open, { desc = "Open parent directory" })
+    end,
+  },
 }
