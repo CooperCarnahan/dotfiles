2f9b7c5fbbf264a2cf7c016a03a47ed3a5f1e74c
CooperCarnahan <coopcarn@gmail.com>
2023-10-03 15:32:34 -0500

    feat(nvim): delete nvim config

--- .config/nvim/ftplugin/c.lua
+++
@@ -1,3 +0,0 @@
-vim.api.nvim_buf_set_keymap(0, "n", "<leader>cb", "<cmd>CMakeBuild<CR>", { desc = "CMakeBuild" })
-vim.api.nvim_buf_set_keymap(0, "n", "<leader>cg", "<cmd>CMakeGenerate<CR>", { desc = "CMakeGenerate" })
-vim.api.nvim_buf_set_keymap(0, "n", "<leader>tc", "<cmd>CMakeOpen<CR>", { desc = "CMakeOpen" })
