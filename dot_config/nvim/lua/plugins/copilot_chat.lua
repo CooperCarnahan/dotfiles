3f1f180bb8cf40d3e9f5fa72b7fe5c595ccb2ddc
CooperCarnahan <coopcarn@gmail.com>
2025-09-15 17:00:52 -0500

    feat(nvim): always add '#buffers' to copilot chat

---
+++ dot_config/nvim/lua/plugins/copilot_chat.lua
@@ -0,0 +1,12 @@
+return {
+  {
+    "CopilotC-Nvim/CopilotChat.nvim",
+    opts = {
+      sticky = {
+        "#buffers",
+      },
+      -- See Configuration section for options
+    },
+    -- See Commands section for default commands if you want to lazy load on them
+  },
+}
