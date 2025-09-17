0e4208639fe897245c6590756d9b4fa8c8b4c1d1
CooperCarnahan <coopcarn@gmail.com>
2025-09-17 14:07:34 -0500

    fix(nvim): only use cmd on windows

--- dot_config/nvim/lua/config/options.lua
+++ dot_config/nvim/lua/config/options.lua
@@ -2,7 +2,12 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
-vim.opt.shell = "cmd"
+if vim.fn.has("win32") == 1 then
+  vim.opt.shell = "cmd"
+else
+  vim.opt.shell = "bash"
+end
+
 vim.cmd("abb u8 UINT8")
 vim.cmd("abb u64 UINT64")
 vim.cmd("abb u32 UINT32")
